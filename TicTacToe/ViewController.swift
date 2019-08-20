4b2de472868ee934610e2434c56e8f63b2b55a6b
Anthony Cianfrocco <afc4328@rit.edu>
2019-08-20 19:55:16 -0400

    Initial Commit

---
+++ TicTacToe/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TicTacToe
+//
+//  Created by Anthony Cianfrocco on 8/20/19.
+//  Copyright © 2019 Anthony Cianfrocco. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
